8090bc657fef58a4b25eae41bd5965555c64f5dc
Grecu Bogdan <BogdanOctavian.Grecu@softwire.com>
2022-06-29 15:43:22 +0100

    filter, map, forEach, etc :)

--- dist/index.js
+++ dist/index.js
@@ -4,7 +4,6 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const fs_1 = __importDefault(require("fs"));
-const { XMLParser, XMLBuilder, XMLValidator } = require("../node_modules/fast-xml-parser");
 var log4js = require("log4js");
 var logger = log4js.getLogger("Transactions");
 log4js.configure({
@@ -48,29 +47,23 @@ class Person {
 function readCSVFile(filename) {
     let fs = require("fs");
     let textByLine = fs.readFileSync(filename).toString().split("\r\n");
-    console.log(textByLine);
     let res = textByLine.map((line) => line.split(","));
-    console.log(res);
     res = res.slice(1);
-    console.log(res);
     return res;
 }
 function readJSONFile(filename) {
     let fs = require("fs");
     let transactions = JSON.parse(fs.readFileSync(filename).toString());
-    console.log(transactions);
-    let transactionsString = [];
-    for (let i = 0; i < transactions.length; i++) {
+    let transactionsString = transactions.map((transaction) => {
         let transactionString = [];
-        console.log(transactions[i]);
-        transactionString.push(transactions[i]["Date"]);
-        transactionString.push(transactions[i]["FromAccount"]);
-        transactionString.push(transactions[i]["ToAccount"]);
-        transactionString.push(transactions[i]["Narrative"]);
-        transactionString.push(transactions[i]["Amount"]);
-        transactionsString.push(transactionString);
-    }
-    console.log(transactionsString);
+        console.log(transaction);
+        transactionString.push(transaction["Date"]);
+        transactionString.push(transaction["FromAccount"]);
+        transactionString.push(transaction["ToAccount"]);
+        transactionString.push(transaction["Narrative"]);
+        transactionString.push(transaction["Amount"]);
+        return transactionString;
+    });
     return transactionsString;
 }
 function readXMLFile(filename) {
@@ -81,80 +74,71 @@ function readXMLFile(filename) {
         ignoreAttributes: false,
         attributeNamePrefix: "@_"
     });
-    //console.log(transactions);
     let parsedData = parser.parse(transactions);
-    let transactionsList = parsedData["TransactionList"]["SupportTransaction"];
-    let transactionsString = [];
-    for (let i = 0; i < transactionsList.length; i++) {
+    let transactionsList = parsedData.TransactionList.SupportTransaction;
+    let transactionsString = transactionsList.map((transaction) => {
         let transactionString = [];
-        //console.log(transactionsList[i]);
-        let parsedDate = new Date((parseInt(transactionsList[i]["@_Date"]) - 25568) * 86400000);
-        console.log(transactionsList["@_Date"]);
+        let msInADay = 86400000;
+        let daysBetween1900And1970 = 25568;
+        let parsedDate = new Date((parseInt(transaction["@_Date"]) - daysBetween1900And1970) * msInADay);
         transactionString.push(parsedDate.toString());
-        transactionString.push(transactionsList[i]["Parties"]["From"]);
-        transactionString.push(transactionsList[i]["Parties"]["To"]);
-        transactionString.push(transactionsList[i]["Description"]);
-        transactionString.push(transactionsList[i]["Value"].toString());
-        transactionsString.push(transactionString);
-    }
-    console.log(transactionsString);
+        transactionString.push(transaction.Parties.From);
+        transactionString.push(transaction.Parties.To);
+        transactionString.push(transaction.Description);
+        transactionString.push(transaction.Value.toString());
+        return transactionString;
+    });
     return transactionsString;
 }
 function parseDataArray(data_array) {
-    let records = [];
-    for (let i = 0; i < data_array.length; i++) {
-        let date = data_array[i][0];
+    let records = data_array.map((element) => {
+        let index = data_array.indexOf(element) + 1;
+        let date = element[0];
         let dateOkCSV = date.match(/[0-9].[0-9].\/[0-9].[0-9].\/[0-9].[0-9].[0-9].[0-9]./g);
         let dateOkJSON = date.match(/[0-9].[0-9].[0-9].[0-9].-[0-9].[0-9]-[0-9].[0-9].T[0-9].[0-9].:[0-9].[0-9].:[0-9].[0-9]./g);
         let dateOkXML = date.match(/[A-Z].[a-z].[a-z]. [A-Z].[a-z].[a-z]. [0-9].[0-9]. [0-9].[0-9].:[0-9].[0-9].:[0-9].[0-9]. GMT\+0100 \(British Summer Time\)/g);
         if (!dateOkCSV && !dateOkJSON && !dateOkXML) {
-            logger.log("Error", "Wrong Date, check line " + i);
+            logger.log("Error", "Wrong Date, check line " + index);
         }
-        let to = data_array[i][1];
-        let from = data_array[i][2];
-        let narrative = data_array[i][3];
-        if (isNaN(parseFloat(data_array[i][4]))) {
-            logger.log("Error", "Invalid Amount, check line " + i);
+        let to = element[1];
+        let from = element[2];
+        let narrative = element[3];
+        if (isNaN(parseFloat(element[4]))) {
+            logger.log("Error", "Invalid Amount, check line " + index);
         }
-        let sum = parseFloat(data_array[i][4]);
+        let sum = parseFloat(element[4]);
         let record = new Record(date, to, from, narrative, sum);
-        records.push(record);
-    }
+        return record;
+    });
     return records;
 }
 function evalTransactions(transactions) {
     let people = [];
-    for (let i = 0; i < transactions.length; i++) {
-        let searchFrom = 0;
-        let negativeAmount = transactions[i].sum * (-1);
-        for (let j = 0; j < people.length; j++) {
-            if (transactions[i].from == people[j].name) {
-                people[j].transaction(negativeAmount);
-                people[j].addTransaction(transactions[i]);
-                searchFrom = 1;
-                break;
-            }
-        }
-        if (searchFrom == 0) {
-            let newPerson = new Person(transactions[i].from, negativeAmount);
-            newPerson.addTransaction(transactions[i]);
+    transactions.forEach((transaction) => {
+        let negativeAmount = transaction.sum * (-1);
+        let searchFrom = people.filter((person) => transaction.from == person.name);
+        if (searchFrom.length == 0) {
+            let newPerson = new Person(transaction.from, negativeAmount);
+            newPerson.addTransaction(transaction);
             people.push(newPerson);
         }
-        let searchTo = 0;
-        for (let j = 0; j < people.length; j++) {
-            if (transactions[i].to == people[j].name) {
-                people[j].transaction(transactions[i].sum);
-                people[j].addTransaction(transactions[i]);
-                searchTo = 1;
-                break;
-            }
+        else {
+            let indexOfFrom = people.indexOf(searchFrom[0]);
+            people[indexOfFrom].transaction(negativeAmount);
+            people[indexOfFrom].addTransaction(transaction);
         }
-        if (searchTo == 0) {
-            let newPerson = new Person(transactions[i].to, negativeAmount);
-            newPerson.addTransaction(transactions[i]);
+        let searchTo = people.filter((person) => transaction.to == person.name);
+        if (searchTo.length == 0) {
+            let newPerson = new Person(transaction.to, transaction.sum);
+            newPerson.addTransaction(transaction);
             people.push(newPerson);
         }
-    }
+        else {
+            let indexOfTo = people.indexOf(searchTo[0]);
+            people[indexOfTo].transaction(transaction.sum);
+            people[indexOfTo].addTransaction(transaction);
+        }
+    });
     return people;
 }
 let running = 1;
@@ -162,12 +146,12 @@ let people = [];
 while (running) {
     let answer = rl.question('Give a command ');
     if (answer == "List All") {
-        for (let i = 0; i < people.length; i++) {
-            process.stdout.write(people[i].name);
+        people.forEach((person) => {
+            process.stdout.write(person.name);
             process.stdout.write(" ");
-            process.stdout.write(people[i].money.toString());
+            process.stdout.write(person.money.toString());
             console.log();
-        }
+        });
     }
     else if (answer == "Exit") {
         running = 0;
@@ -177,15 +161,16 @@ while (running) {
         if (text[0] == "List") {
             let name = answer.substring(5);
             console.log(name);
-            for (let j = 0; j < people.length; j++) {
-                if (people[j].name == name) {
-                    process.stdout.write(people[j].name);
-                    process.stdout.write(" ");
-                    process.stdout.write(people[j].money.toString());
-                    console.log();
-                    console.log(people[j].transactions);
-                    break;
-                }
+            let foundPerson = people.filter((person) => person.name == name);
+            if (foundPerson.length == 0) {
+                logger.log("Warning", "User not found");
+            }
+            else {
+                process.stdout.write(foundPerson[0].name);
+                process.stdout.write(" ");
+                process.stdout.write(foundPerson[0].money.toString());
+                console.log();
+                console.log(foundPerson[0].transactions);
             }
         }
         else if (text[0] == "Add") {
@@ -212,9 +197,6 @@ while (running) {
             }
         }
     }
-    logger.log("debug", "proba");
+    logger.log("Debug", "Ok");
 }
-//let data_array = readFile("./DodgyTransactions2015.csv");
-//let transactions = parseDataArray(data_array);
-//let people = evalTransactions(transactions);
 //# sourceMappingURL=index.js.map
